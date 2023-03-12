a9ed42440f8d36b8826f7f00c47c6233097666f1
ashraf-a-khan <khan.ashraf2311@gmail.com>
2023-03-12 16:49:10 -0400

    Working on BlogList, sanity studio done

---
+++ app/(user)/page.tsx
@@ -0,0 +1,40 @@
+import { previewData } from "next/headers";
+import { client } from "../../lib/sanity.client";
+import PreviewSuspense from "../../components/PreviewSuspense"
+
+import { groq } from "next-sanity";
+import BlogList from "../../components/BlogList";
+import PreviewBlogList from "../../components/PreviewBlogList";
+
+
+
+
+const query = groq`*[_type=='post'] {
+    ...,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
+ `;
+ 
+
+
+export default async function HomePage() {
+    if (previewData()) {
+        return (<PreviewSuspense fallback={
+            <div role="status">
+            <p className="text-center text-lg animate-pulse text-[#F7AB0A]">
+              Loading Preview Data...
+            </p>
+          </div>
+        }>
+            <PreviewBlogList query={query} />
+        </PreviewSuspense>);
+    }
+
+
+    const posts = await client.fetch(query);
+
+    return (
+       <BlogList posts={posts} />
+    );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function Header() {
+    return (
+        <header className="flex items-center justify-between space-x-2 font-bold px-10 py-5">
+            <div>
+                <Link href="/">
+                    <Image
+                        className="rounded-full"
+                        height={50}
+                        width={50}
+                        src="https://www.shutterstock.com/shutterstock/photos/755744683/display_1500/stock-vector-concept-of-blogging-golden-blog-word-bubble-message-in-pop-art-comic-style-755744683.jpg"
+                        alt="logo"
+                    />
+                </Link>
+                <h1>ASHRAF ONLINE JOURNAL</h1>
+            </div>
+            <div>
+                <Link
+                    href="https://ashraf-a-khan.github.io/Portfolio/"
+                    className="px-5 py-3 text-sm md:text-base bg-gray-900 text-[#F7AB0A] flex items-center rounded-full text-center"
+                >
+                    View My Portfolio
+                </Link>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
