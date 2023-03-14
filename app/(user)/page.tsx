9cf4c8d4fde9e1f82da74346a653110c28e9d532
ashraf-a-khan <khan.ashraf2311@gmail.com>
2023-03-13 20:37:40 -0400

    Commit before deployment

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,13 +1,12 @@
 import { previewData } from "next/headers";
 import { client } from "../../lib/sanity.client";
-import PreviewSuspense from "../../components/PreviewSuspense"
+import PreviewSuspense from "../../components/PreviewSuspense";
 
 import { groq } from "next-sanity";
 import BlogList from "../../components/BlogList";
 import PreviewBlogList from "../../components/PreviewBlogList";
 
-
-
+export const revalidate = 30; // revalidate this page every 30 seconds
 
 const query = groq`*[_type=='post'] {
     ...,
@@ -15,26 +14,25 @@ const query = groq`*[_type=='post'] {
     categories[]->
   } | order(_createdAt desc)
  `;
- 
-
 
 export default async function HomePage() {
     if (previewData()) {
-        return (<PreviewSuspense fallback={
-            <div role="status">
-            <p className="text-center text-lg animate-pulse text-[#F7AB0A]">
-              Loading Preview Data...
-            </p>
-          </div>
-        }>
-            <PreviewBlogList query={query} />
-        </PreviewSuspense>);
+        return (
+            <PreviewSuspense
+                fallback={
+                    <div role="status">
+                        <p className="text-center text-lg animate-pulse text-[#F7AB0A]">
+                            Loading Preview Data...
+                        </p>
+                    </div>
+                }
+            >
+                <PreviewBlogList query={query} />
+            </PreviewSuspense>
+        );
     }
-
 
     const posts = await client.fetch(query);
 
-    return (
-       <BlogList posts={posts} />
-    );
+    return <BlogList posts={posts} />;
 }
