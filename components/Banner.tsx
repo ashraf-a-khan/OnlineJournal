010404f39c3bb8a804fcf5876ad0c98a4085987f
ashraf-a-khan <khan.ashraf2311@gmail.com>
2023-03-13 22:11:40 -0400

    Change home page content

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,13 +3,14 @@ function Banner() {
         <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
             <div>
                 <h1 className="text-7xl">Ashraf's Daily Blog</h1>
-                <h2 className="mt-5 md:mt-0">
-                    Welcome to{" "}
-                    <span className="underline decoration-4 decoration-[#F7AB0A]">
-                        Every Developers
-                    </span>{" "}
-                    favourite blog in the Devosphere
-                </h2>
+                <h3 className="mt-5 md:mt-0">
+                    Welcome to my Blog. This is a fully responsive app that is
+                    powered by Sanity studio v3 Content Management System with
+                    full TypeScript support. This provides the embedded ability
+                    to have a studio in the app along with the power of NextJS
+                    13. Feel free to play around and view some of the projects I
+                    have worked on. Also, don't forget to add me on LinkedIn.
+                </h3>
             </div>
             <p className="mt-5 md:mt-2 text-gray-400 max-w-sm">
                 New Projects | The latest clones | Weekly news updates |
